7a12c3c37d887085056d47e1403ce99f01d107ba
DaeYoung Jeong <eodud1025@naver.com>
2025-07-27 20:57:44 +0900

    chore: 프로젝트 세팅

---
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.todo
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.todo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.visibleBtn.setOnClickListener {
+            binding.targetView.visibility = View.VISIBLE
+        }
+
+        binding.invisibleBtn.setOnClickListener {
+            binding.targetView.visibility = View.INVISIBLE
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
